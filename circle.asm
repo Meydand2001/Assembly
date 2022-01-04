132730a2a195811d3eb3e923fbea196745e54585
Yehonatan314 <63951392+Yehonatan314@users.noreply.github.com>
2022-01-04 20:42:19 +0200

    Update circle.asm

--- circle.asm
+++ circle.asm
@@ -16,7 +16,7 @@ mac $t2, $t1, $imm1, $t0, -8, 0								  # $t2 = $t0 - 8*$t1 = y
 sub $t2, $t2, $imm1, $zero, 128, 0              # $t1 = y - 128
 mac $t1, $t1, $t1, $zero, 0, 0									# $t1 = $t1^2 = (x-128)^2
 mac $t2, $t2, $t2, $t1, 0, 0										# $t2 = (x-128)^2 +(y-128)^2
-blt $zero, $t2, $a0, $imm1, 0, CALC						  # if ((x-128)^2 +(y-128)^2 < r) jump to CALC
+bgt $zero, $t2, $a0, $imm1, 0, CALC						  # if ((x-128)^2 +(y-128)^2 > r) jump to CALC
 
 COLOR:
 out $zero, $imm1, $zero, $t0, 20, 0							# montioraddr = $t0 = (x,y)

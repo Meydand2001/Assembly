99ed3d9d3ace879ed192bc2f5f1b4ba76d14e1b2
Yehonatan314 <63951392+Yehonatan314@users.noreply.github.com>
2022-01-04 21:01:58 +0200

    Update circle.asm

--- circle.asm
+++ circle.asm
@@ -7,16 +7,16 @@ add $t0, $imm1, $zero, $zero, -1, 0						  # $t0 = -1
 sll $s0, $imm1, $imm2, $zero, 1, 16             # $s0 = 65536
 
 CALC:
-beq $zero, $t0, $s0, $imm2, 0, RIP				      # if $t0 = $s0 then RIP
+bge $zero, $t0, $s0, $imm2, 0, RIP				      # if $t0 = $s0 then RIP
 add $t0, $t0, $imm1, $zero, 1, 0								# $t0 += 1
-srl $t1, $t0, $imm1, $zero, 8, 0								# $t1 = $t1 : 256 rounded down
+srl $t1, $t0, $imm1, $zero, 8, 0								# $t1 = $t0 : 256 rounded down
 sll $t1, $t1, $imm1, $zero, 8, 0                # $t1 = $t1 * 256
-sub $t1, $t0, $t1, $imm1, 128, 0                # $t1 = $t0 mod 256 - 128 = x -128
-mac $t2, $t1, $imm1, $t0, -8, 0								  # $t2 = $t0 - 8*$t1 = y
-sub $t2, $t2, $imm1, $zero, 128, 0              # $t1 = y - 128
+sub $t1, $t0, $t1, $imm1, -128 , 0              # $t1 = $t0 mod 256 - 128 = x -128
+mac $t2, $t1, $imm1, $t0, -256, 0								# $t2 = $t0 - 256*$t1 = y
+srl $t2, $t0, $imm1, $zero, 8, 0								# $t2 = $t0 : 256 rounded down
 mac $t1, $t1, $t1, $zero, 0, 0									# $t1 = $t1^2 = (x-128)^2
 mac $t2, $t2, $t2, $t1, 0, 0										# $t2 = (x-128)^2 +(y-128)^2
-bgt $zero, $t2, $a0, $imm1, 0, CALC						  # if ((x-128)^2 +(y-128)^2 > r) jump to CALC
+bgt $zero, $t2, $a0, $imm1, 0, CALC						  # if ((x-128)^2 +(y-128)^2 > r^2) jump to CALC
 
 COLOR:
 out $zero, $imm1, $zero, $t0, 20, 0							# montioraddr = $t0 = (x,y)

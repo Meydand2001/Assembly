a7432a75121a9c63be63c49bf0e59a36db8b38fb
Yehonatan314 <63951392+Yehonatan314@users.noreply.github.com>
2022-01-04 19:51:10 +0200

    Update circle.asm

--- circle.asm
+++ circle.asm
@@ -1,6 +1,7 @@
 .word 0x100 20
 
-add $a0, $imm1, $zero, $zero, 0x100, 0					# $a0 = radius
+lw $a0, $imm1, $zero, $zero, 0x100, 0					  # $a0 = MEM[0x100] = radius
+add $a1, $imm1, $zero, $zero, 128, 0            # $a1 = center point
 add $t0, $imm1, $zero, $zero, -1, 0						  # $t0 = -1
 
 CALC:
@@ -8,17 +9,18 @@ bgt $zero, $t0, $imm1, $imm2, 65535, RIP				# if $t0
 add $t0, $t0, $imm1, $zero, 1, 0								# $t0 += 1
 srl $t1, $t0, $imm1, $zero, 8, 0								# $t1 = $t1 : 256 rounded down
 sll $t1, $t1, $imm1, $zero, 8, 0                # $t1 = $t1 * 256
-sub $t1, $t0, $t1, $zero, 0, 0                  # $t1 = $t0 mod 256 = x
+sub $t1, $t0, $t1, $imm1, 128, 0                # $t1 = $t0 mod 256 - 128 = x -128
 mac $t2, $t1, $imm1, $t0, -8, 0								  # $t2 = $t0 - 8*$t1 = y
-mac $t1, $t1, $t1, $zero, 0, 0									# $t1 = $t1^2 = x^2
-mac $t2, $t2, $t2, $t1, 0, 0										# $t2 = x^2 +y^2
-bgt $zero, $t2, $a0, $imm1, 0, CALC						  # if (x^2 +y^2 >r) jump to CALC
+sub $t2, $t2, $imm1, $zero, 128, 0              # $t1 = y - 128
+mac $t1, $t1, $t1, $zero, 0, 0									# $t1 = $t1^2 = (x-128)^2
+mac $t2, $t2, $t2, $t1, 0, 0										# $t2 = (x-128)^2 +(y-128)^2
+blt $zero, $t2, $a0, $imm1, 0, CALC						  # if ((x-128)^2 +(y-128)^2 < r) jump to CALC
 
 COLOR:
-out $zero, $imm1, $zero, $t0, 20, 0							# montioraddr = $t0 =(x,y)
+out $zero, $imm1, $zero, $t0, 20, 0							# montioraddr = $t0 = (x,y)
 out $zero, $imm1, $zero, $imm2, 21, 255				  # montiordata = 255 = white
 out $zero, $imm1, $zero, $imm2, 22, 1					  # montiorcmd = 1 = color pixel
-reti $zero, $zero, $zero, $zero, 0, 0						# return
+beq $zero, $zero, $zero, $imm1, CALC, 0					# jump to CALC
 
 RIP:
 halt $zero, $zero, $zero, $zero, 0, 0						# rest in peace dear circle

4dcfef384e1ee9341ff738b140533f18cf2ac30b
Yehonatan314 <63951392+Yehonatan314@users.noreply.github.com>
2022-01-04 19:13:43 +0200

    Update binom.asm

--- binom.asm
+++ binom.asm
@@ -3,7 +3,7 @@
 
 lw $s0, $imm1, $zero, $zero, 0x100, 0							# $s0 = n
 lw $s1, $imm1, $zero, $zero, 0x101, 0							# $s1 = k
-add $v0, $zero, $zero, $zero, 0, 0									# $v0 = 0
+add $v0, $zero, $zero, $zero, 0, 0								# $v0 = 0
 sll $sp, $imm1, $imm2, $zero, 1, 11		            # set $sp = 1 << 11 = 2048
 add $ra, $imm1, $zero, $zero, DIEBITCH, 0         # $ra = DIEBITCH
 
@@ -13,20 +13,22 @@ sw $s0, $sp, $imm1, $zero, 0, 0										# MEM[$sp + 0] = $s0
 sw $s1, $sp, $imm1, $zero, 1, 0										# MEM[$sp + 1] = $s1
 sw $ra, $sp, $imm1, $zero, 2, 0										# MEM[$sp + 2] = $ra
 beq $zero, $s1, $zero, $imm1, CALC, 0							# if [k==0] jump to CALC
-beq $zero, $s1, $s0, $imm1, CALC, 0							# if [k==n] jump to CALC
+beq $zero, $s1, $s0, $imm1, CALC, 0							  # if [k==n] jump to CALC
 add $s0, $s0, $imm1, $zero, -1, 0									# $s0 = $s0 - 1
-jal $ra, $zero, $zero, $imm1, BINOM, 0 							# jump to BINOM
-add $v0, $v0, $imm1, $zero, 1, 0									# $v0 += 1
+jal $ra, $zero, $zero, $imm1, BINOM, 0 						# jump to BINOM
 add $s1, $s1, $imm1, $zero, -1, 0									# $s1 = $s1 - 1
-jal $ra, $zero, $zero, $imm1, BINOM, 0 							# jump to BINOM
-add $v0, $v0, $imm1, $zero, 1, 0									# $v0 += 1
+jal $ra, $zero, $zero, $imm1, BINOM, 0 						# jump to BINOM
 
-CALC:
+RELEASE:
 lw $s0, $sp, $imm1, $zero, 0, 0										# $s0 = MEM[$sp + 0]
 lw $s1, $sp, $imm1, $zero, 1, 0										# $s1 = MEM[$sp + 1]
 lw $ra, $sp, $imm1, $zero, 2, 0										# $ra = MEM[$sp + 2]
 add $sp, $sp, $imm1, $zero, 3, 0									# $sp += 3
-beq $zero, $zero, $zero, $ra, 0, 0									# jump to $ra
+beq $zero, $zero, $zero, $ra, 0, 0								# jump to $ra
+
+CALC:
+add $v0, $v0, $imm1, $zero, 1, 0									# $v0 += 1
+beq $zero, $zero, $zero, $imm1, RELEASE, 0				# jump to RELEASE:
 
 DIEBITCH:
 sw $v0, $imm1, $zero, $zero, 0x102, 0             # MEM[0x102] = $v0

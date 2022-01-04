ac44e711b1310104372ed9c28c0751d9275ce3fd
gildro02 <96247470+gildro02@users.noreply.github.com>
2022-01-04 18:36:49 +0200

    Update binom.asm

--- binom.asm
+++ binom.asm
@@ -7,10 +7,10 @@ add $v0, $zero, $zero, $zero, 0, 0									# $v0 = 0
 sll $sp, $imm1, $imm2, $zero, 1, 11		            # set $sp = 1 << 11 = 2048
 
 BINOM:
-add $sp, $sp, $imm1, $zero, -3, 0									# $sp -= 12
+add $sp, $sp, $imm1, $zero, -3, 0									# $sp -= 3
 sw $s0, $sp, $imm1, $zero, 0, 0										# MEM[$sp + 0] = $s0
-sw $s1, $sp, $imm1, $zero, 1, 0										# MEM[$sp + 4] = $s1
-sw $ra, $sp, $imm1, $zero, 2, 0										# MEM[$sp + 8] = $ra
+sw $s1, $sp, $imm1, $zero, 1, 0										# MEM[$sp + 1] = $s1
+sw $ra, $sp, $imm1, $zero, 2, 0										# MEM[$sp + 2] = $ra
 beq $zero, $s1, $zero, $imm1, CALC, 0							# if [k==0] jump to CALC
 beq $zero, $s1, $s0, $imm1, CALC, 0							# if [k==n] jump to CALC
 add $s0, $s0, $imm1, $zero, -1, 0									# $s0 = $s0 - 1
@@ -22,8 +22,8 @@ add $v0, $imm1, $zero, $zero, 1, 0									# $v0 += 1
 
 CALC:
 lw $s0, $sp, $imm1, $zero, 0, 0										# $s0 = MEM[$sp + 0]
-lw $s1, $sp, $imm1, $zero, 1, 0										# $s1 = MEM[$sp + 4]
-lw $ra, $sp, $imm1, $zero, 2, 0										# $ra = MEM[$sp + 8]
-add $sp, $sp, $imm1, $zero, 3, 0									# $sp += 12
+lw $s1, $sp, $imm1, $zero, 1, 0										# $s1 = MEM[$sp + 1]
+lw $ra, $sp, $imm1, $zero, 2, 0										# $ra = MEM[$sp + 2]
+add $sp, $sp, $imm1, $zero, 3, 0									# $sp += 3
 beq $zero, $zero, $zero, $ra, 0, 0									# jump to $ra
 halt $zero, $zero, $zero, $zero, 0, 0
